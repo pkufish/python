9cbcd7b69ec0db79851166897e6cbc5ab0f68e07
wangzuocai@gmail.com <sk2i3@#92>
2024-01-02 13:57:18 +0800

    11

---
+++ google trends/get info.py
@@ -0,0 +1,10 @@
+from pytrends.request import TrendReq
+
+pytrends = TrendReq(hl='en-US', tz=360)
+
+kw_list = ["Blockchain"]
+pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='', gprop='')
+
+pytrends.interest_over_time()
+
+
